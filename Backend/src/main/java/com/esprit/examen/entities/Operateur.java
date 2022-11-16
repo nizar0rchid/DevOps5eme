10062395c77a266c89e89a08526be2fef50b9a1b
Derbel <74288509+yassine-der@users.noreply.github.com>
2022-11-17 00:31:13 +0100

    Update Operateur.java

--- Backend/src/main/java/com/esprit/examen/entities/Operateur.java
+++ Backend/src/main/java/com/esprit/examen/entities/Operateur.java
@@ -34,5 +34,8 @@ public class Operateur implements Serializable{
 	@OneToMany
 	@JsonIgnore
 	private Set<Facture> factures;
-	
+	public Operateur(String nom, String prenom, String password){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.password = password;
 }
