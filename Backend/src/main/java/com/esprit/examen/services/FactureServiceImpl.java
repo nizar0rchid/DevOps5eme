6b69f698740c9130853a9f996e6977222f76c321
Talbi Achref <58572823+talbi21@users.noreply.github.com>
2022-11-17 15:30:28 +0100

    Update FactureServiceImpl.java

--- Backend/src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ Backend/src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -59,7 +59,7 @@ public class FactureServiceImpl implements IFactureService {
 		float montantRemise = 0;
 		for (DetailFacture detail : detailsFacture) {
 			//Récuperer le produit 
-			Produit produit = produitRepository.findById(detail.getProduit().getIdProduit()).get();
+			Produit produit = produitRepository.findById(detail.getProduit().getIdProduit()).orElse(null);
 			//Calculer le montant total pour chaque détail Facture
 			float prixTotalDetail = detail.getQteCommandee() * produit.getPrix();
 			//Calculer le montant remise pour chaque détail Facture
@@ -100,7 +100,10 @@ public class FactureServiceImpl implements IFactureService {
 	@Override
 	public List<Facture> getFacturesByFournisseur(Long idFournisseur) {
 		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
+
+	   assert (fournisseur!=null);
 		return (List<Facture>) fournisseur.getFactures();
+
 	}
 
 	@Override
