f764d7fde31e11ddf72864d5bd06345a9ffdb072
Talbi Achref <58572823+talbi21@users.noreply.github.com>
2022-11-17 15:48:30 +0100

    Update FactureServiceImpl.java

--- Backend/src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ Backend/src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -112,10 +112,11 @@ public class FactureServiceImpl implements IFactureService {
 	public void assignOperateurToFacture(Long idOperateur, Long idFacture) {
 		Facture facture = factureRepository.findById(idFacture).orElse(null);
 		Operateur operateur = operateurRepository.findById(idOperateur).orElse(null);
-		operateur.getFactures().add(facture);
-		operateurRepository.save(operateur);
+		if(operateur != null) {
+			operateur.getFactures().add(facture);
+			operateurRepository.save(operateur);
+		}
 	}
-
 	@Override
 	public float pourcentageRecouvrement(Date startDate, Date endDate) {
 		float totalFacturesEntreDeuxDates = factureRepository.getTotalFacturesEntreDeuxDates(startDate,endDate);
